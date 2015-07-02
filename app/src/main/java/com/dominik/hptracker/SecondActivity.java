3e477eaa02ecea8ba2dc194b83e3e54bfa22b96b
Daniel <cowwithhead@gmail.com>
2015-07-02 15:52:47 -0600

    Created new activity, added button to move from main to second activity.

--- app/src/main/java/com/dominik/hptracker/SecondActivity.java
+++ app/src/main/java/com/dominik/hptracker/SecondActivity.java
@@ -1,39 +1,22 @@
 package com.dominik.hptracker;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.JsonReader;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity
+public class SecondActivity extends ActionBarActivity
 {
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button myButton = (Button) findViewById(R.id.button1);
-        myButton.setOnClickListener(new View.OnClickListener()
-        {
-
-            @Override
-            public void onClick(View v)
-            {
-                startActivity (new Intent(MainActivity.this, SecondActivity.class));
-            }
-        });
-    }
-
-    public void buttonOnClick(View v)
-    {
-        Button button = (Button) v;
-        button.setText("clicked");
+        setContentView(R.layout.activity_second);
     }
 
     @Override
